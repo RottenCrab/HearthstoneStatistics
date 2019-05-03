a07703886b0f1c7c060a25a9a15b5de62bda9ac2
RottenCrab <ndoureliadis.aueb@gmail.com>
2019-05-03 04:25:24 +0300

    Create a function to generate REPORT.md

--- src/reporting_tool.py
+++ src/reporting_tool.py
@@ -1,10 +1,22 @@
-from card_pool import CardPool
-from dt_viz import (avg_stats,
-                     probabilities)
+from string import Template
+from .json_handler import read_json
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-def reporting():
-    pass
-
+def reporting(total_card_pool_standard,
+              total_card_pool_wild):
+    template_dictionary = read_json('image_urls.json',
+                                    json_key='all_keys')
+    template_dictionary['total_card_pool_standard'] = total_card_pool_standard
+    template_dictionary['total_card_pool_wild'] = total_card_pool_wild
+    filein = open('../Templates/REPORT_TEMPLATE.md')
+    out = open('../Reports/REPORT.md', 'w')
+    src = Template(filein.read())
+    result = src.safe_substitute(template_dictionary)
+    out.write(result)
+    filein.close()
+    out.close()
 
 # end of file
